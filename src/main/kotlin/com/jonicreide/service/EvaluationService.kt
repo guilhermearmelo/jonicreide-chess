55d2ce4b04661b4a445443beab51fc0f70bcd22f
Guilherme.Melo <Guilherme.Melo@AB-inbev.com>
2024-02-19 21:35:45 -0300

    Plan Material + Mobility Evaluation

--- src/main/kotlin/com/jonicreide/service/EvaluationService.kt
+++ src/main/kotlin/com/jonicreide/service/EvaluationService.kt
@@ -33,4 +33,20 @@ class EvaluationService() {
 
         return whiteMaterial - blackMaterial
     }
+
+    // todo Material + Mobility Evaluation
+
+    // todo Claude Shannon Single Node Evaluation
+    // f(p) = 200(K-K')
+    //       + 9(Q-Q')
+    //       + 5(R-R')
+    //       + 3(B-B' + N-N')
+    //       + 1(P-P')
+    //       - 0.5(D-D' + S-S' + I-I')
+    //       + 0.1(M-M') + ...
+    //
+    // KQRBNP = number of kings, queens, rooks, bishops, knights and pawns
+    // D,S,I = doubled, blocked and isolated pawns
+    // M = Mobility (the number of legal moves)
+
 }
